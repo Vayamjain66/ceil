2dbbb8d59bcaaedf0cefca77bd7c9ab51278920b
Vayamjain66 <154872628+Vayamjain66@users.noreply.github.com>
2025-04-16 15:47:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+function ceil(number) {
+  return Math.ceil(number);
+}
+
+app.post('/functions/ceil', (req, res) => {
+  const number = req.body.input;
+
+  if (typeof number !== 'number') {
+    return res.status(400).json({ error: 'Input must be a number.' });
+  }
+
+  const result = ceil(number);
+  res.json({ output: result });
+});
+
+app.get('/functions/ceil', (req, res) => {
+  res.json({
+    name: 'ceil',
+    description: 'Rounds a number UP to the nearest integer.',
+    input: {
+      type: 'number',
+      description: 'A number to round up.',
+      example: 3.14
+    },
+    output: {
+      type: 'number',
+      description: 'The smallest integer greater than or equal to the input number.',
+      example: 4
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
